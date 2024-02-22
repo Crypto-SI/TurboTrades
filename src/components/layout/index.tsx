e99fc35c5f7b55014b7bd3589acba805339cb461
venuswhispers <venuswhisperer412@gmail.com>
2024-02-22 16:35:55 +1000

    chore: code refacotring

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -16,7 +16,7 @@ const Header = dynamic(() => import("@/components/layout/header"), {
 const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <div className="p-4">
-      <div className="-z-10 p-4 bg-[url('/images/bg-light.svg')] dark:bg-[url('/images/bg-dark.svg')] bg-[#F7F7FB] dark:bg-[#030303] bg-cover bg-no-repeat w-[100vw] h-[100vh] fixed"></div>
+      <div className="-z-10 p-4 bg-[url('/images/bg-light.svg')] dark:bg-[url('/images/bg-dark.svg')] bg-[#F7F7FB] dark:bg-[#030303] bg-cover bg-no-repeat top-0 left-0 right-0 bottom-0 fixed"></div>
       <Header />
       <div className="flex mt-2 flex-col md:flex-row">
         <Sider />
